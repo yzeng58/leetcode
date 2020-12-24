5debaacd3497e7eae3b99139c06c75c144ecb7ab
yzeng58 <yzeng58@wisc.edu>
2020-12-23 19:07:34 -0600

    Dec 23

--- python/modules.py
+++ python/modules.py
@@ -24,7 +24,7 @@ class ListNode:
     
 def linkedList(lists):
     """
-    Generate a linked list.
+    Generate a linked list through a lists.
     """
     prehead = ListNode(-1)
 
@@ -36,6 +36,15 @@ def linkedList(lists):
     return prehead.next
 
 def test(inputs, func):
+    """
+    Tests whether the given function works correctly via printing
+    the output of the function given the arguments.
+    
+    Parameters:
+    -----------
+    inputs: a list of inputs/arguments. e.g. [[*args1], [*args2]]
+    func: function
+    """
     start = time()
     for args in inputs:
         output = ""
