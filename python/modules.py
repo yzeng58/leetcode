9a84d9ae1731e85dadf484958972dfe5aa30ed81
yzeng58 <yzeng58@wisc.edu>
2020-12-24 19:07:07 -0600

    Dec 24

--- python/modules.py
+++ python/modules.py
@@ -21,6 +21,71 @@ class ListNode:
     
     def __str__(self):
         return self.__repr__()
+
+from graphviz import Graph, Digraph
+class binaryTreeNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.left = left
+        self.right = right
+        self.val = val
+        
+    def to_graphviz(self, g = None):
+        if g == None:
+            g = Digraph()
+            
+        # draw self
+        g.node(repr(self.val))
+    
+        for label, child in [("0", self.left), ("1", self.right)]:
+            if child != None:
+                # draw child, recursively
+                child.to_graphviz(g)
+                
+                # draw edge from self to child
+                g.edge(repr(self.val), repr(child.val))
+        return g
+    
+    def _repr_svg_(self):
+        return self.to_graphviz()._repr_svg_()
+    
+    def listToTree(nums):
+        root = None
+        
+        for num in nums:
+            if root == None:
+                root = binaryTreeNode(num)
+                continue
+            
+            current = root
+            while current != None:
+                if current.val > num:
+                    if current.left == None:
+                        current.left = binaryTreeNode(num)
+                        break
+                    else:
+                        current = current.left
+                else:
+                    if current.right == None:
+                        current.right = binaryTreeNode(num)
+                        break
+                    else:
+                        current = current.right
+                        
+        return root
+    
+    def sort(self): 
+        nums = []
+        current = self
+        
+        if current.left != None:
+            nums += current.left.sort()
+        
+        nums.append(current.val)
+            
+        if current.right != None:
+            nums += current.right.sort()
+            
+        return nums
     
 def linkedList(lists):
     """
